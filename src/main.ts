d040cfa29946a73b6ad605147dd8f9ad9c21b528
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-10-25 11:28:04 -0300

    Add window module for setting panel properties and update main.ts to use it

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import { createApp } from "vue";
+import { setPanelProperties } from "./common/window";
 import App from "./App.vue";
+import { Position } from "@tauri-apps/plugin-positioner";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+setPanelProperties(Position.BottomRight);
+app.mount("#app");
