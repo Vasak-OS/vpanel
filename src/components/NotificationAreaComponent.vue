d3e69dd28120d47a0706509c3ba1096202345a42
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-10-28 17:25:20 -0300

    Add NotificationAreaComponent with ClockComponent

---
+++ src/components/NotificationAreaComponent.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import ClockComponent from './ClockComponent.vue';
+</script>
+
+<template>
+  <div class="navale-notification-area">
+    <ClockComponent />
+  </div>
+</template>
