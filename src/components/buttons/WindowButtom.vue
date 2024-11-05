2a1bcc04e8351474b81c4da74c938fdc7aa487af
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-11-05 18:58:26 -0300

    Agregar componente WindowButton para gestionar ventanas y mostrar iconos en base64

---
+++ src/components/buttons/WindowButtom.vue
@@ -0,0 +1,49 @@
+<script lang="ts" setup>
+import { invoke } from "@tauri-apps/api/core";
+import { getIcon } from "../../common/icons";
+import { onMounted, ref } from "vue";
+
+const iconBase64 = ref<string>("");
+
+const { id, title, is_minimized, icon } = defineProps({
+  id: {
+    type: String,
+    required: true,
+  },
+  title: String,
+  is_minimized: Boolean,
+  icon: {
+    type: String,
+    default: "",
+  },
+});
+
+const focusWindow = async (windowId: string) => {
+  try {
+    await invoke("focus_window", { windowId });
+  } catch (error) {
+    console.error("Error focusing window:", error);
+  }
+};
+
+onMounted(async () => {
+  iconBase64.value = await getIcon(icon);
+});
+</script>
+<template>
+  <div
+    class="px-3 py-1 hover:bg-gray-700 rounded cursor-pointer flex items-center gap-2"
+    :class="{ 'opacity-50': is_minimized }"
+    @click="focusWindow(id)"
+  >
+    <img v-if="icon" :src="`data:image/svg+xml;base64,${iconBase64}`" class="w-4 h-4 icon" :alt="title" />
+    <span>{{ title }}</span>
+  </div>
+</template>
+
+<style>
+.icon {
+  width: 26px;
+  height: 26px;
+}
+</style>
