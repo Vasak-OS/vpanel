8cf37510497c5d98d5c920525328d59522ca3238
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-11-05 19:52:44 -0300

    Agregar archivo de estilos CSS y su integración en main.ts para soporte de Tailwind

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import { setPanelProperties } from "./common/window";
 import App from "./App.vue";
+import "./style.css";
 import { Position } from "@tauri-apps/plugin-positioner";
 
 const app = createApp(App);
