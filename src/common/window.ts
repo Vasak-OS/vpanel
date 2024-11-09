500ae756e2f16a219f8ef34e7495b89869c2fa48
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-11-09 17:05:12 -0300

    Eliminar parámetros innecesarios en setPanelProperties y simplificar la configuración del tamaño de la ventana

--- src/common/window.ts
+++ src/common/window.ts
@@ -1,18 +1,12 @@
 
 import { getCurrentWindow, LogicalSize } from "@tauri-apps/api/window";
-import { moveWindow, Position } from "@tauri-apps/plugin-positioner";
-
 const thisWindow = await getCurrentWindow();
 
 
-const setPanelProperties = async (position: Position) => {
+const setPanelProperties = async () => {
   try {
     const { width: screenWidth } = await window.screen;
-
     await thisWindow.setSize(new LogicalSize(screenWidth, 32));
-    await thisWindow.setMaxSize(new LogicalSize(screenWidth, 32));
-    await thisWindow.setMinSize(new LogicalSize(screenWidth, 32));
-    await moveWindow(position);
   } catch (error) {
     console.error("Error configurando la ventana como panel:", error);
   }
