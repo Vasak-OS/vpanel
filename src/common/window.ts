f6857e48065b4a64579a99b1c75a25def996744a
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2025-04-04 17:59:13 -0300

    Refactorización de la configuración de la ventana en window.ts
    - Mueve la obtención de la ventana actual dentro de la función setPanelProperties para mejorar la encapsulación.
    - Elimina la exportación innecesaria de thisWindow, optimizando el código.

--- src/common/window.ts
+++ src/common/window.ts
@@ -1,14 +1,12 @@
 import { getCurrentWindow, LogicalSize } from "@tauri-apps/api/window";
 
-const thisWindow = await getCurrentWindow();
-
 export const setPanelProperties = async (): Promise<void> => {
   try {
+    const thisWindow = await getCurrentWindow();
     const { width: screenWidth } = await window.screen;
     await thisWindow.setSize(new LogicalSize(screenWidth, 32));
   } catch (error) {
     console.error("[Panel Error] Error configurando la ventana:", error);
   }
 };
-export default thisWindow;
 
