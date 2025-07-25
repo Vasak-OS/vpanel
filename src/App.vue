af16d5f503439ede014fc72daafa3b6ad12a72f9
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2025-07-25 15:17:28 -0300

    Refactor: mejorar la consistencia del código y aplicar estilos en el componente App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,49 @@
 <script setup lang="ts">
 import { onMounted, onUnmounted, ref, type Ref } from "vue";
-import { Command } from '@tauri-apps/plugin-shell'
+import { Command } from "@tauri-apps/plugin-shell";
 import { setPanelProperties } from "@/common/window";
-import WindowsArea from '@/components/WindowsArea.vue';
-import TrayPanel from '@/components/TrayPanel.vue';
-import ClockComponent from '@/components/ClockComponent.vue';
-import { getIconSource } from '@vasakgroup/plugin-vicons'
+import WindowsArea from "@/components/WindowsArea.vue";
+import TrayPanel from "@/components/TrayPanel.vue";
+import ClockComponent from "@/components/ClockComponent.vue";
+import { getIconSource } from "@vasakgroup/plugin-vicons";
 import { useConfigStore } from "@vasakgroup/plugin-config-manager";
 import { listen } from "@tauri-apps/api/event";
 
-const menuIcon: Ref<string> = ref('');
-const notifyIcon: Ref<string> = ref('');
+const menuIcon: Ref<string> = ref("");
+const notifyIcon: Ref<string> = ref("");
 const configStore = useConfigStore();
 let unlistenConfig: Function | null = null;
 
 const setMenuIcon = async () => {
   try {
-    menuIcon.value = await getIconSource('menu-editor');
+    menuIcon.value = await getIconSource("menu-editor");
   } catch (err) {
-    console.error('Error: finding icon menu')
+    console.error("Error: finding icon menu");
   }
-}
+};
 
 const setNotifyIcon = async () => {
   try {
-    notifyIcon.value = await getIconSource('preferences-desktop-notification')
+    notifyIcon.value = await getIconSource("preferences-desktop-notification");
   } catch (err) {
-    console.error('Error: finding notify icon')
+    console.error("Error: finding notify icon");
   }
-}
+};
 
 const openMenu = () => {
-  Command.create('vmenu').execute()
-}
+  Command.create("vmenu").execute();
+};
 
 const openNotificationCenter = () => {
-  Command.create('vasak-control-center').execute()
-}
+  Command.create("vasak-control-center").execute();
+};
 
 onMounted(async () => {
   setMenuIcon();
   setNotifyIcon();
   await setPanelProperties();
   configStore.loadConfig();
-  unlistenConfig = await listen('config-changed', async () => {
+  unlistenConfig = await listen("config-changed", async () => {
     configStore.loadConfig();
   });
 });
@@ -56,16 +56,18 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <nav
-    class="vpanel">
-    <img :src="menuIcon"  alt="Menu" @click="openMenu"
-      class="app-icon" />
+  <nav class="vpanel background">
+    <img :src="menuIcon" alt="Menu" @click="openMenu" class="app-icon" />
     <WindowsArea />
     <div class="flex content-center items-center">
       <TrayPanel />
       <ClockComponent />
-      <img :src="notifyIcon"  alt="Menu" @click="openNotificationCenter"
-      class="app-icon" />
+      <img
+        :src="notifyIcon"
+        alt="Menu"
+        @click="openNotificationCenter"
+        class="app-icon"
+      />
     </div>
   </nav>
 </template>
@@ -74,7 +76,7 @@ onUnmounted(() => {
 @reference "./style.css";
 
 .vpanel {
-  @apply flex justify-between items-center mx-1 bg-white/70 dark:bg-black/70 hover:bg-white/80 hover:dark:bg-black/80;
+  @apply flex justify-between items-center mx-1 hover:bg-white/80 hover:dark:bg-black/80;
   height: 30px;
   padding: 2px;
   border-radius: calc(var(--border-radius) + 2px);
